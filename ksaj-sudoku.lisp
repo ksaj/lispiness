ef41a1c73a08c32e49dccce6d170b464de5cc6e6
Karsten Johansson <KarstenJohansson@gmail.com>
2018-08-25 19:08:40 -0400

    Improved the listp tests

--- ksaj-sudoku.lisp
+++ ksaj-sudoku.lisp
@@ -10,7 +10,7 @@ Right now, does nothing but create and test the grid.
 TODO:	- Subject to change on a whim
 -----
 DONE	- Set up grid and initial (test-grid) 
-	- Improve the listp tests 
+DONE	- Improve the listp tests 
 	- (reset-grid) - zero out the matrix
 	- (print-game) - output in standard game format
 	- (test-game) - populate board w/ a build-in solvable game
@@ -37,20 +37,26 @@ DONE	- Set up grid and initial (test-grid)
 
 (defun test-grid ()
     "Call (test-grid) to run some tests from REPL"
-    (print (aref sudoku-grid 0 0))
+    (format t "~%~%Set up cell tests:~%")
     (setf (aref sudoku-grid 0 0) 3)
-    (print (aref sudoku-grid 0 1))
+    (print (aref sudoku-grid 0 0))
     (setf (aref sudoku-grid 0 1) '(0 1 3))
+    (print (aref sudoku-grid 0 1))
     (setf (aref sudoku-grid 0 2) 0)
-    (print (aref sudoku-grid 0 3))
+    (print (aref sudoku-grid 0 2))
     (setf (aref sudoku-grid 0 3) '(4))  
     (print (aref sudoku-grid 0 3))
+    (setf (aref sudoku-grid 0 4) '())
+    (print (aref sudoku-grid 0 4))
+    (terpri)
     (print sudoku-grid)
-    (listp (aref sudoku-grid 0 0))	; 3 should be  NIL 
-    (listp (aref sudoku-grid 0 1))	; (0 1 3) should be T   
-    (listp (aref sudoku-grid 0 2))	; 0 should be NIL 
-    (listp (aref sudoku-grid 0 3))	; (4) should be T
-    )					; On separate line to facilitate additions
+    (format t "~%~%listp tests:~%")
+    (format t "3 Pass if NIL: ~a~%" (listp (aref sudoku-grid 0 0)))	; 3 should be  NIL 
+    (format t "\(0 1 3\) Pass if T: ~a~%"(listp (aref sudoku-grid 0 1)))		; (0 1 3) should be T   
+    (format t "0 Pass if NIL: ~a~%" (listp (aref sudoku-grid 0 2)))	; 0 should be NIL 
+    (format t "\(4\) Pass if T: ~a~%" (listp (aref sudoku-grid 0 3)))		; (4) should be T
+    (format t "\(\) Pass if T: ~a~%" (listp (aref sudoku-grid 0 4)))		; () should be T
+    )	; On separate line to facilitate additions
 
 (test-grid)				; DEBUG
 
