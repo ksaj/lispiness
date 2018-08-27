dfeb769d8515897d63de9dbbafe4bcdac020dcc4
Karsten Johansson <KarstenJohansson@gmail.com>
2018-08-27 19:55:27 -0400

    Create equal-or-thereabouts.lisp

---
+++ equal-or-thereabouts.lisp
@@ -0,0 +1,12 @@
+#|
+
+This will be about EQ, EQUAL, =, string=, etc, as well as pluralization.
+
+eg:
+
+(format nil "~D tr~:@P/~D win~:P" 7 1)
+(format nil "~D tr~:@P/~D win~:P" 1 0)
+(format nil "~D tr~:@P/~D win~:P" 1 3)
+
+|#
+
