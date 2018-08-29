422b6ebd0f08d7df890e97c3557fb584729700a2
Karsten Johansson <KarstenJohansson@gmail.com>
2018-08-29 02:15:19 -0400

    Found an interesting font rendering difference.
    Compare the wonky tab indents in (info) from vi versus the output when called in the clisp REPL.

--- ksaj-sudoku.lisp
+++ ksaj-sudoku.lisp
@@ -1,4 +1,7 @@
-#|    Karsten Johansson's Sudoku Solver  v0.1
+#|8-0
+
+
+      Karsten Johansson's Sudoku Solver  v0.1
       =======================================
 
 Soon to be yet another sudoku solver. It'll pretty much mimic how
@@ -12,18 +15,26 @@ TODO:	- Subject to change on a whim
 DONE	- Set up grid and initial (test-grid) 
 DONE	- Start (info)
 COMING	- (test-game) - populate board w/ a built-in solvable game
-	- (print-game) - output in standard game format
+	- (print-game) - output sudoku-grid in standard game format
+	- (solve-game)
 	- (load-game '<filename>) 
-	- (solve-game) 
 	- Loop count to abort when stuck in puzzle we can't solve
         - Load specific or random game from multi-game text file
+	- (prompts) - prompt user for input args
 	- Enable command line executable functionality 
 	- MPI
-|#
+													0-8|#
+
+
+
+;;;
+;;; Game Configuration and Testing Stuff
+;;;
 
 (defun test-grid ()				; Delete function and (info) reference when no longer needed.
     "Call (test-grid) to run some tests from REPL. Remove from v1.0."
     (format t "~%Set up cell tests:~%")
+    (reset-grid)				; Start off clean
     (setf (aref sudoku-grid 0 0) 3)		; This is what a solved cell looks like
     (print (aref sudoku-grid 0 0))
     (setf (aref sudoku-grid 0 1) '(1 3))	; This is what a cell with 2 candidates looks like
@@ -49,15 +60,17 @@ COMING	- (test-game) - populate board w/ a built-in solvable game
 
 (defun info ()
     "Help Screen"
-    (format t "~%Karsten Johansson's Sudoku Solver  v0.1~%")
+    (format t "~%~%Karsten Johansson's Sudoku Solver  v0.1~%")
     (format t "---------------------------------------~%~%")
     (format t "(reset-grid)		- Blank game grid~%")
     (format t "(test-grid)		- Run some tests~%")	; Will be deleted when no longer needed
-    (format t "(test-easy-game)		- Populate with sample easy game~%")
-    (format t "(test-hard-game)		- Populate with sample hard game (Not yet...)~%")
+    (format t "(test-easy-game)	- Populate with sample easy game~%")
+    (format t "(test-hard-game)	- Populate with sample hard game (Not yet...)~%")
     (format t "(load-game)		- Not yet...~%")
+    (format t "(fix-ocd-indent-trigger)	 - No..~%")
     (format t "(solve-game)		- One can only hope and dream...~%")
     (format t "(print-game)		- Not yet...~%")
+    (format t "(quit)			- Quitter!~%")
     (format t "sudoku-grid		- Display the current game grid~%~%")
     t)
 
@@ -65,8 +78,11 @@ COMING	- (test-game) - populate board w/ a built-in solvable game
     "Create a blank sudoku grid with all cells set to NIL"
     (setq sudoku-grid (make-array '(9 9))))
 
+;; A couple of test games to test with
+
 (defun test-easy-game ()
     "Populate grid with an easy test game"	; Ugly as sin. Turn into a loop, mapcar, or whatevs
+    (reset-grid)
     (setf (aref sudoku-grid 0 1) 7)
     (setf (aref sudoku-grid 0 3) 3)
     (setf (aref sudoku-grid 0 5) 6)
@@ -104,6 +120,7 @@ COMING	- (test-game) - populate board w/ a built-in solvable game
 
 (defun test-hard-game ()			; Test for hard solve functions
     "Populate grid with a hard test game"
+    (reset-grid)
    ;((0 1 2) (1 3 6) (1 8 3) (2 1 7) (2 2 4) (2 4 8)		; (ROW COL NUM)
    ; (3 5 2) (3 8 2) (4 1 8) (4 4 4) (4 7 1) (5 0 6) (5 3 5)	; Hard sample from
    ; (6 4 1) (6 6 7) (6 7 8) (7 0 5) (7 5 9) (8 7 4))		; "A SAT-based Sudoku Solver"
@@ -112,7 +129,9 @@ COMING	- (test-game) - populate board w/ a built-in solvable game
     (print "Coming soon...")
     )
 
-;; Solve Strategies
+;;;
+;;; Solve Strategies
+;;;
 
 (defun sure-thing-rows ()	; will eventally run concurrently
     "Search rows for cases with only one possible candidate. Return solution to pencil"
@@ -150,12 +169,11 @@ COMING	- (test-game) - populate board w/ a built-in solvable game
     (print "coming Soon...")
     )
 
-;;;
-;;; The Action Begins...
-;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;                        ;;;
+;;;  The Action Begins...  ;;;
+;;;                        ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(reset-grid)			; Start with a blank grid
-
-sudoku-grid
-
+(info)								; Gotta start somewhere...
 
