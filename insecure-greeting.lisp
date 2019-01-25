f555400c710fdc7095f2d20265a93adafd27872e
Karsten Johansson <KarstenJohansson@gmail.com>
2019-01-25 17:41:33 -0500

    Update and rename 1.lisp to insecure-greeting.lisp

--- insecure-greeting.lisp
+++ insecure-greeting.lisp
@@ -1,14 +1,3 @@
-;;;; Basically I haven't written LISP since the early
-;;;; 90's and have decided to get back to it since I
-;;;; was always able to get results just about as fast
-;;;; as I could think up potential solutions.
-;;;;
-;;;; Go ahead and glean what you will from my 
-;;;; re-progress. Since Lisp has grown, I'm starting
-;;;; over. I'll include notes from really important
-;;;; stuff I remember from 'way_back_when. Including
-;;;; the security warning at the end of this first 
-;;;; program.
 
 (setq *print-case* :capitalize)         ; pretty looking input
 
@@ -27,11 +16,11 @@
 ;;;; WARNING
 ;;;; -------
 ;;;;
-;;;; If you enter something like: myname rm 1.lisp
+;;;; If you enter something like: myname rm insecure-greeting.lisp
 ;;;; it will write: Hello Myname
 ;;;; and then delete itself. I shit you not.
 ;;;;
 ;;;; Quotes make it work right, but this is a very important gotcha.
 ;;;; Suck it up and don't do this in production code. Bad things will 
-;;;; happen.  Welcome to Secure Coding 101.
+;;;; happen.
 ;;;;
