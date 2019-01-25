60d87b32e8b2f4ed8ab520eb6339af2853690d8b
Karsten Johansson <KarstenJohansson@gmail.com>
2019-01-25 17:38:00 -0500

    Create zodiac.lisp

---
+++ zodiac.lisp
@@ -0,0 +1,26 @@
+; Coming soon
+
+; Just stashing some notes here for now, so this isn't lispy at all yet
+
+
+
+
+
+Siderial Zodiac
+---------------
+Capricorn: Jan 20
+Aquarius: Feb 16
+Pisces: March 11
+Aries: April 18
+Taurus: May 13
+Gemini: June 21
+Cancer: July 20
+Leo: Aug 10
+Virgo: Sept 16
+Libra: Oct 30
+Scorpio: Nov 23
+Ophiuchus: Nov 29
+Sagittarius: Dec 17
+
+... a man wrestling a serpent, dividing the snake’s body in two parts. As such, Ophiuchus is considered a healer of men, a doctor of medicine, or a scientist who seeks higher education and enlightenment.  He’s also an interpreter of dreams, favoured by his father and other authority figures, and expected to achieve a high position in life.
+
