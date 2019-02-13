dd9307b58ea091cbd2af22993bae013206c0b833
Karsten Johansson <KarstenJohansson@gmail.com>
2019-02-13 17:33:54 -0500

    Create to-research.lisp

---
+++ to-research.lisp
@@ -0,0 +1,20 @@
+;;;;
+;;;; Stuff found in lisp repos that need further research
+;;;;
+
+
+;; Why is this a thing? What is its purpose?
+(defun copy-cons (c)
+  (cons (car c) (cdr c)))
+
+; (copy-cons '(a b c)) => (A B C)
+
+
+;; 
+
+
+
+
+
+
+
